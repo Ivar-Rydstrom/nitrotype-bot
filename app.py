f4056d726dc10ca3b4ed368dcf844ab366680997
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2019-03-27 22:18:25 -0700

    added detection and response functionality

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import pytesseract
+import pyautogui
+
+pytesseract.pytesseract.tesseract_cmd = r'E:\Ivar\Apps\Tesseract\tesseract.exe'
+
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=300)
+    return processed_img
+
+
+def get_char(img):
+    text = pytesseract.image_to_string(img)
+    return text
+
 
 while(True):
-    printscreen_pil = ImageGrab.grab()
-    printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8')\
-        .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
-    cv2.imshow('window', printscreen_numpy)
+    screen = np.array(ImageGrab.grab(bbox=(1920 / 3 + 60, 2.05 * 1080 / 3, 2 * 1920 / 3 - 70, 1.58 * 1080 / 2)))
+    new_screen = process_img(screen)
+    detection = get_char(screen)
+    if detection:
+        char = detection[0]
+        pyautogui.keyDown(char)
+        pyautogui.keyUp(char)
+    print(char)
+    # cv2.imshow('window', new_screen)
+    cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
