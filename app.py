84264a6604dab22f70d686a11e6fb72adf5aecba
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2019-03-27 19:16:44 -0700

    initial screen grab functionality

---
+++ app.py
@@ -0,0 +1,12 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+
+while(True):
+    printscreen_pil = ImageGrab.grab()
+    printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8')\
+        .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
+    cv2.imshow('window', printscreen_numpy)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
